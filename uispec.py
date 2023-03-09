3c603324212e236378af04fef0e84bb0fb9ea668
AvijitPradhan <127403408+AvijitPradhan@users.noreply.github.com>
2023-03-10 03:09:55 +0530

    Update uispec.py

--- uispec.py
+++ uispec.py
@@ -1,3 +1,8 @@
 from behave import *
 from selenium.webdriver.common.keys import keys
 
+@given("User is able to navigate to https://reqres.in/")
+ def step_impl(context):
+    driver.get('https://reqres.in/')
+    
+ pass
