527ea2a027feb78cf1f58a9b5fba6bb64bf0dcf5
AvijitPradhan <127403408+AvijitPradhan@users.noreply.github.com>
2023-03-10 14:23:04 +0530

    Update apispec.py

--- apispec.py
+++ apispec.py
@@ -1,1 +1,22 @@
 
+import requests
+
+@given("User has all the prerequisites for POST and GET requests")
+
+*** Environment files should be initialized ***
+
+@When ("User sends POST requests")
+
+import requests
+url = "/api/users/"
+payload =
+"{\"user\":{\"email\":\"admin@xyz.com\",\"password\":\"admin123 4\"}}"
+headers = {
+'accept': “application/json”,
+...}
+response = requests.request("POST", url, data=payload, headers=headers, verify=False)
+print(response.text)
+
+ @then ("User should be created")
+ 
+import odbc
