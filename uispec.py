fc9532033e926a352595b75d20abd870190a9192
AvijitPradhan <127403408+AvijitPradhan@users.noreply.github.com>
2023-03-15 16:44:55 +0530

    Update uispec.py

--- uispec.py
+++ uispec.py
@@ -1,18 +1,24 @@
 from behave import *
 from selenium.webdriver.common.keys import keys
+from selenium.webdriver.chrome.service import Service
+from selenium import webdriver
 
 @given("User is able to navigate to https://reqres.in/")
  def step_impl(context):
+    service = Service(executable_path="/usr/local/bin/chromedriver")
+    with webdriver.Chrome(service=service) as driver:
     driver.get('https://reqres.in/')
     
 @when ("User taps on any request on homepage")
  def step_impl(context):
-    driver.click('button')
+    element = driver.find_element_by_id("users")
+    element.click()
    
 @then("Then User see different request types and endpoints")
- pass
-.
-.
+ def step_impl(context):
+    element = driver.find_element_by_id("users", "users-single", "users-single-not-found","unknown", "unknown-single","unknown-single-not-found", "post","put","patch", "delete")
+    element.click()
+    
 @then("Then User can see a Button to navigate to Support Page")
  def step_impl(context):
   select = select(driver.find_element(byname,'supportpage')
