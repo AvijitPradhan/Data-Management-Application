9818ab941bbf3e93eb2ef159f4d4aae5d4262e72
AvijitPradhan <127403408+AvijitPradhan@users.noreply.github.com>
2023-03-15 17:38:28 +0530

    Update uispec.py

--- uispec.py
+++ uispec.py
@@ -11,22 +11,47 @@ from selenium import webdriver
     
 @when ("User taps on any request on homepage")
  def step_impl(context):
-    element = driver.find_element_by_id("users")
-    element.click()
+    context.element = driver.find_element_by_id("users")
+    context.element.click()
    
-@then("Then User see different request types and endpoints")
+@then("User see different request types and endpoints")
  def step_impl(context):
-    element = driver.find_element_by_id("users", "users-single", "users-single-not-found","unknown", "unknown-single","unknown-single-not-found", "post","put","patch", "delete")
-    element.click()
+    context.element = driver.find_element_by_id("users", "users-single", "users-single-not-found","unknown", "unknown-single","unknown-single-not-found", "post","put","patch", "delete")
+    context.element.click()
     
-@then("Then User can see a Button to navigate to Support Page")
+@then("user can see Sample request and response details for different request types")
  def step_impl(context):
-  select = select(driver.find_element(byname,'supportpage')
-                  
-@then("User can see 'One Time' and 'Monthly' payment  option in Support page")
+  m = driver.find_element_by_xpath("//span[@class='url']")
+#verify if element present
+  b = m.is_displayed()
+   if b:
+    print("Element with span class available")
+   else:
+    print("Element with span class not available")
+   context.response = driver.find_element_by_xpath(//*[@class='response-code'])
+   print(context.response)
+  
+  context.responsedata = driver.find_element_by_xpath(//*[contains(@data-key,'output-response')
+   print(context.responsedata)                                                       
+                                                   
+@then("User can see a Button to navigate to Support Page")
+ def step_impl(context):
+   context.element = driver.find_element_by_id("support-heading")                                                    
+    print(context.element())
+                                                          
+@then("user see One Time payment option in Support page")
   def step_impl(context):
-   select = select.select_by_index(index)
- 
-@And("user see the Upgrade button options")
-    def step_impl(context):
-     element = driver.find_element(By.name, "source")
+   context.button =driver.find_element_by_xpath("//input[@id='supportOneTime']")
+   context.element.click()
+                                                          
+@then("user see Monthly payment option in Support page")
+  def step_impl(context):
+   context.button =driver.find_element_by_xpath("//input[@id='supportRecurring']")
+                                                          
+@and("user sees the Upgrade button options after support page")
+   def step_impl(context):
+    context.element = driver.find_element_by_id("trigger-pro")                                                    
+    context.element.click()                                                      
+                                                          
+                                                          
+                                                          
